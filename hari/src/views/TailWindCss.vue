2eac43e3798084bbd79af9810c88101e3756e2c0
hariharan047 <shariharan2233@gmail.com>
2024-01-26 11:36:30 +0530

    Practice vue concepts and tailwindcss is used for styling

---
+++ hari/src/views/TailWindCss.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-9xl bg-blue-200 text-end">Hari</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TailWindCss",
+};
+</script>
+
+<style></style>
